71200c7e779326c62eff31ce083d32f82bdd47da
unusable <unusable@163.com>
2019-12-30 18:01:46 +0800

    refine generate config

---
+++ Assets/Scripts/Lua/Editor/GenCustomTypes.cs
@@ -0,0 +1,27 @@
+// version: 1
+using System.Collections.Generic;
+using System;
+public static class GenCustomTypes
+{
+    [XLua.LuaCallCSharp] public static IEnumerable<Type> LuaCallCSharp { get { return List; } }
+    [XLua.CSharpCallLua] public static List<Type> CSharpCallLua { get { return GenConfig.CSharpCallLua(List); } }
+    private static System.Type[] List = {
+
+// typeof(BindAnchor),
+// typeof(ExampleGenConfig),
+typeof(LuaBehaviour),
+// typeof(LuaBehaviour.BindData),
+// typeof(LuaCallCs),
+// typeof(LuaCore),
+// typeof(Tutorial.BaseClass),
+// typeof(Tutorial.ByFile),
+// typeof(Tutorial.ByString),
+// typeof(Tutorial.CSCallLua),
+// typeof(Tutorial.CSCallLua.DClass),
+// typeof(Tutorial.CustomLoader),
+// typeof(Tutorial.DerivedClass),
+// typeof(Tutorial.DerivedClassExtensions),
+// typeof(Tutorial.Param1),
+
+    };
+}
